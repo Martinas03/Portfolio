08f92190586f2389f657e07ed4ca0d0e4980536f
Martinas <martinas1990@mail.ru>
2023-08-21 18:10:31 +0300

    added github to Projects.js

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -6,6 +6,7 @@ import Title from '../comon/components/Title';
 import todoImage from './../assets/images/todolist.png'
 import counter from './../assets/images/counter.png'
 import socialImage from './../assets/images/social_network.png'
+import github from './../assets/images/github.jpeg'
 import {GoProject} from "react-icons/go";
 import Fade from 'react-reveal/Fade';
 
@@ -22,6 +23,10 @@ const Projects = () => {
         backgroundImage: `url(${counter})`,
     };
 
+   const githubAccount = {
+        backgroundImage: `url(${github})`,
+    };
+
     return (
         <div id='projects' className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`}>
@@ -35,14 +40,14 @@ const Projects = () => {
                             <Project href={'https://martinas03.github.io/react-samurai/'} style={social} title={'Social network'} description={'Cool social network'}/>
                         </Fade>
                         <Fade right>
-
                             <Project href={'https://martinas03.github.io/Todolist/'} style={todoList} title={'Todolist'} description={'Cool todolist'}/>
                         </Fade>
                         <Fade left>
-
                             <Project href={'https://martinas03.github.io/my_counter/'} style={myCounter} title={'Counter'} description={'Cool counter'}/>
                         </Fade>
-
+                        <Fade right>
+                            <Project href={'https://github.com/Martinas03'} style={githubAccount} title={'My github'} description={'My github'}/>
+                        </Fade>
                     </div>
                 </div>
 
